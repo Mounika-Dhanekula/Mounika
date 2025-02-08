82fb6c10c11185b8f83cece7ecd5e3135e60325d
Mounika-Dhanekula <mounikad621@gmail.com>
2025-02-08 22:58:42 +0530

    Add files via upload

---
+++ 07_listoperation (1).py
@@ -0,0 +1,10 @@
+"""Create a Python list that contains the names of 5 different fruits. Perform the given operations on
+the list"""
+fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"]
+print("List of fruits:", fruits)
+fruits.append("Grapes")  
+print("After adding Grapes:", fruits)
+fruits.remove("Banana")  
+print("After removing Banana:", fruits)
+print("The first fruit in the list is:", fruits[0])  
+print("The last fruit in the list is:", fruits[-1]) 
