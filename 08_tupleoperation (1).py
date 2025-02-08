5079a585417f963cf525b7e8bdf6787cc501f0e4
Mounika-Dhanekula <mounikad621@gmail.com>
2025-02-08 23:00:33 +0530

    Add files via upload

---
+++ 08_tupleoperation (1).py
@@ -0,0 +1,7 @@
+"""Write a Python program that creates a tuple containing 5 numbers. Perform the given operations
+on the tuple"""
+numbers = (10, 20, 30, 40, 50)
+print("Tuple of numbers:", numbers)
+print("First element of the tuple:", numbers[0])
+print("Last element of the tuple:", numbers[-1]) 
+print("Length of the tuple:", len(numbers)) 
