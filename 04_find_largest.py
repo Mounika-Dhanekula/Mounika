b357650edf4a5737ac53c7981b93013d1e90f669
Mounika-Dhanekula <mounikad621@gmail.com>
2025-02-08 22:57:04 +0530

    Update and rename 04_find_largest.py to 04_f_strings

--- 04_find_largest.py
+++
@@ -1,7 +0,0 @@
-"""Write a Python function called find_largest() that takes a list of numbers as input and returns the
-largest number from the list. Test the function with a sample list"""
-def find_largest(numbers_list):
-    return max(numbers_list)
-sample_list = [12, 45, 78, 23, 56]
-largest_number = find_largest(sample_list)
-print("The largest number in the list is:", largest_number)
